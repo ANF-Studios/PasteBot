aa9046a18369223a14e28a4e3450e9987cde7263
ANF-Studios <theanfstudios@gmail.com>
2021-01-19 11:07:04 -0500

    Fixes #2 | Added a C++ bot

---
+++ CPP/src/main.cpp
@@ -0,0 +1,15 @@
+#include <discpp/client.h>
+#include <discpp/context.h>
+#include <discpp/command_handler.h>
+#include <discpp/client_config.h>
+
+int main(int argc, const char* argv[]) {
+	auto* config = new discpp::ClientConfig({"!"});
+	discpp::Client bot{ "TOKEN_HERE", config };
+
+	discpp::Command ping_command("ping", "Quick example of a command", {}, [](discpp::Context ctx) {
+		ctx.Send("pong!");
+	}, {});
+
+	return bot.Run();
+}
